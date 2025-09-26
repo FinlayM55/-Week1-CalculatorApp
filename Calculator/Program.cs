b46c4ed32d08d47f1d2c7830ef46d8a954d7b99d
Finlay <Morgan-F5@ulster.ac.uk>
2025-09-26 21:32:45 +0100

    try 3

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,9 +4,9 @@ int Firstnumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Type in the second number followed by the Enter key");
 int Secondnumber = Convert.ToInt32(Console.ReadLine());
 
-int result = Firstnumber + Secondnumber;
+int Result = Firstnumber + Secondnumber;
 
-Console.WriteLine("Adding {0} and {1} give the answer {2}", Firstnumber, Secondnumber, result);
+Console.WriteLine("Adding {0} and {1} give the answer {2}", Firstnumber, Secondnumber, Result);
 
 
 
