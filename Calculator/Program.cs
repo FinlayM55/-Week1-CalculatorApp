d80c659996b5004f9545c44d90c7ee43cb6fdb8a
Finlay <Morgan-F5@ulster.ac.uk>
2025-09-26 22:44:40 +0100

    Updated program code with if-else statements.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,12 +1,51 @@
-﻿Console.WriteLine("Type in the first number followed by the Enter key");
-int Firstnumber = Convert.ToInt32(Console.ReadLine());
+﻿CalculatorApp();
 
-Console.WriteLine("Type in the second number followed by the Enter key");
-int Secondnumber = Convert.ToInt32(Console.ReadLine());
+void CalculatorApp()
+{
 
-int Result = Firstnumber + Secondnumber;
+    int firstnumber = 0;
+    int secondnumber = 0;
+    int result = 0;
+    int choice = 0;
 
-Console.WriteLine("Adding {0} and {1} give the answer {2}", Firstnumber, Secondnumber, Result);
+    Console.WriteLine("Type in the first number followed by the Enter key");
+    firstnumber = Convert.ToInt32(Console.ReadLine());
 
+    Console.WriteLine("Type in the second number followed by the Enter key");
+    secondnumber = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Choose an option from the following list:");
+    Console.WriteLine("1 - Add");
+    Console.WriteLine("2 - Subtract");
+    Console.WriteLine("3 - Divide");
+    Console.WriteLine("4 - Multiply");
+
+    choice = Convert.ToInt32(Console.ReadLine());
+
+    if (choice == 1)
+    {
+        result = firstnumber + secondnumber;
+        Console.WriteLine("Adding {0} and {1} equals {2}", firstnumber, secondnumber,  result);
+    }
+    else if(choice == 2)
+    {
+        result = firstnumber - secondnumber;
+        Console.WriteLine("Subtracting {0} and {1} equals {2}", firstnumber, secondnumber, result);
+    }
+    else if (choice == 3)
+    {
+        result = firstnumber / secondnumber;
+        Console.WriteLine("Dividing {0} and {1} equals {2}", firstnumber, secondnumber, result);
+    }
+    else if (choice == 4)
+    {
+        result = firstnumber * secondnumber;
+        Console.WriteLine("Multiplying {0} and {1} equals {2}", firstnumber, secondnumber, result);
+    }
+
+
+
+    Console.WriteLine("The result is {0}", result);
+}
 
 
